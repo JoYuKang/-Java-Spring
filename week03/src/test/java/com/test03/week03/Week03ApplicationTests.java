3d2a7224be660be8b20fbfd1d5287896ffce0e0c
Yu Knag <ykang9533@naver.com>
2021-07-18 13:35:09 +0900

    # 세팅

---
+++ week03/src/test/java/com/test03/week03/Week03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test03.week03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
