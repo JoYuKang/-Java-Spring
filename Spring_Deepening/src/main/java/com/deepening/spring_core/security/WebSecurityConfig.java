94dba8cb3746897514d40d1bc96426e5a51677af
Yu Knag <ykang9533@naver.com>
2021-08-20 15:59:40 +0900

    # 카카오 로그인 연동

--- Spring_Deepening/src/main/java/com/deepening/spring_core/security/WebSecurityConfig.java
+++ Spring_Deepening/src/main/java/com/deepening/spring_core/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.deepening.spring_core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,5 +45,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling()
                 .accessDeniedPage("/user/forbidden");
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
