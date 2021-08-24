59f1932584250e514c7b43b0fb0994e0b8fb4303
Yu Knag <ykang9533@naver.com>
2021-08-24 21:25:47 +0900

    # JSP

--- Spring_Deepening/src/main/java/com/deepening/spring_core/repository/ProductRepository.java
+++ Spring_Deepening/src/main/java/com/deepening/spring_core/repository/ProductRepository.java
@@ -7,4 +7,13 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByUserId(Long userId);
+    
+    // 상품명이 title인 관심상품 1개 조회
+    Product findByTitle(String title);
+
+    // 상품명이 word가 포함된 모든 상품 조회
+    List<Product> findAllByTitleContaining(String word);
+
+    // 최저가가 fromPrice ~ toPrice인 모든 상품 조회
+    List<Product> findByLpriceBetween(int fromprice, int toprice);
 }
