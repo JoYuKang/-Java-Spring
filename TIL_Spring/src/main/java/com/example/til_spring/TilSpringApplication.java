950a887e1b7b0c16a6b1b23efb0ba8f5d4570ffd
Yu Kang <ykang9533@naver.com>
2021-09-15 11:02:55 +0900

    # 파일 정리

---
+++ TIL_Spring/src/main/java/com/example/til_spring/TilSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.til_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilSpringApplication.class, args);
+    }
+
+}
---
+++ TIL_Spring/src/test/java/com/example/til_spring/TilSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.til_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
