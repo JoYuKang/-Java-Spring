5fc35dd6abb560e0185e710598030d90fc62ad7a
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2021-12-15 15:24:25 +0900

    # UserServiceTest

--- myrestaurant/src/main/java/com/example/myrestaurant/MyrestaurantApplication.java
+++ myrestaurant/src/main/java/com/example/myrestaurant/MyrestaurantApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyrestaurantApplication {
+    public class MyrestaurantApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyrestaurantApplication.class, args);
--- myrestaurant/src/main/java/com/example/myrestaurant/wishlist/repository/WishListRepository.java
+++ myrestaurant/src/main/java/com/example/myrestaurant/wishlist/repository/WishListRepository.java
@@ -2,10 +2,11 @@ package com.example.myrestaurant.wishlist.repository;
 
 import com.example.myrestaurant.db.MemoryDbRepositoryAbstract;
 import com.example.myrestaurant.wishlist.entity.WishListEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository //jpa Repository 나중에 변경하기
-public class WishListRepository  extends JpaRepository<WishListEntity, Long> {
+public class WishListRepository extends JpaRepository<WishListEntity, Long> {
 
 //extends MemoryDbRepositoryAbstract<WishListEntity>
 }
