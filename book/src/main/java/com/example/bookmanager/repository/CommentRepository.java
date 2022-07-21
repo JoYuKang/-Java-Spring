f3f1ae6cb2434b0b83341055d32d36e95e2eff04
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-07-21 23:45:44 +0900

    # 초기화

--- book/src/main/java/com/example/bookmanager/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookmanager.repository;
-
-import com.example.bookmanager.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-
-}
--- book/src/main/java/com/example/bookmanager/repository/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanager.repository;
-
-import com.example.bookmanager.domain.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-}
